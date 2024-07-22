084145eb076152f8f8c723e1772ca26d0aeaf812
AzaleaTopaz <azaleahowland@gmail.com>
2024-07-22 16:00:29 -0400

    Trying to figure out FileField

--- backend/aesthete_project/aesthete/models.py
+++ backend/aesthete_project/aesthete/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     Phone = models.CharField(max_length=200)
     location = models.CharField(max_length=500)
     Image_url = models.URLField(max_length=700, null=True)
-    Portfolio = models.FileField()
+    Portfolio = models.FileField(upload_to='users/portfolios/', null=True, blank=True)
 
     class Meta: 
         db_table = 'users'
@@ -17,11 +17,11 @@ class User(models.Model):
         return self.Name
         
 class Project(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    User = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     Name = models.CharField(max_length=400, default='no name')
     Start_date = models.DateField(blank=True, null=True)
     End_Date = models.DateField(blank=True, null=True)
-    Content = models.FileField(default = 'no content')
+    Content = models.FileField(upload_to='projects/contents/', null=True, blank=True)
     Description = models.TextField(max_length=800, default='no description')
 
     class Meta: 
@@ -29,3 +29,18 @@ class Project(models.Model):
 
     def __str__(self):
         return self.Name
+
+class Review(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    Name = models.CharField(max_length=400)
+    Review_text = models.TextField(max_length=900)
+    Recommend = models.BooleanField(default=False)
+    Date = models.DateField(blank=True)
+
+    class Meta: 
+        db_table = 'reviews'
+
+    def __str__(self):
+        return self.Name
+
+
