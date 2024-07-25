2d5464425f6523860e656f581f3082e2a03c9b75
AzaleaTopaz <azaleahowland@gmail.com>
2024-07-25 12:02:33 -0400

    making a change back to an old commit

--- backend/aesthete_project/aesthete_django/urls.py
+++ backend/aesthete_project/aesthete_django/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('/admin', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('aesthete.urls')),
-    path('/api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
     ]
