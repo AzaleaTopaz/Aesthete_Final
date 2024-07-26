45d37396576b9e84cd0f11f0e0401c7064ab835b
AzaleaTopaz <azaleahowland@gmail.com>
2024-07-26 11:59:16 -0400

    created urls for MediaFile

--- backend/aesthete_project/aesthete/urls.py
+++ backend/aesthete_project/aesthete/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import ListUsers, UserDetail, UserDetailByUsername, ListProjects, ProjectDetail, ListReviews, ReviewDetail
+from .views import ListUsers, UserDetail, UserDetailByUsername, ListProjects, ProjectDetail, ListReviews, ReviewDetail, ListCreateMediaFiles, MediaFileDetail
 
 urlpatterns = [
+    path('media/', ListCreateMediaFiles.as_view(), name='list_create_media_files'),
+    path('media/<int:pk>/', MediaFileDetail.as_view(), name='media_file_detail'),
+    
     path('users/', ListUsers.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('users/username/<str:username>/', UserDetailByUsername.as_view(), name='user-detail-by-username'),
+   
     path('projects/', ListProjects.as_view(), name='project-list'),
     path('projects/<int:pk>/', ProjectDetail.as_view(), name='project-detail'),
+    
     path('reviews/', ListReviews.as_view(), name='review-list'),
     path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail')
 ]
